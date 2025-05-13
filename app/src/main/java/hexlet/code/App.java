3a0c4d8371c5e4865852034dd02f5a5baf851a11
rustam ganiev <psy12@yandex.ru>
2025-05-13 12:14:07 +0500

    added picocli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,25 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import picocli.CommandLine;
+import java.util.concurrent.Callable;
+
+    @CommandLine.Command(
+            name = "gendiff",
+            version = "1.0",
+            mixinStandardHelpOptions = true, //автосправка
+            description = "Compares two configuration files and shows a difference."
+    )
+    
+    public class App implements Callable<Integer> {
+
+        @Override
+        public Integer call() { //логика приложения
+            System.out.println("Hello world!");
+            return 0; // код завершения
+        }
+
+        public static void main(String[] args) {
+            int exitCode = new CommandLine(new App()).execute(args);
+            System.exit(exitCode);
         }
     }
