0b2e3f07d6c2f8abc01c5864cd2182ca3f927543
rustam ganiev <psy12@yandex.ru>
2025-05-13 12:48:39 +0500

    -format added

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,16 +1,43 @@
 package hexlet.code;
 
 import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.File;
 import java.util.concurrent.Callable;
 
-    @CommandLine.Command(
+    @Command(
             name = "gendiff",
             version = "1.0",
             mixinStandardHelpOptions = true, //автосправка
             description = "Compares two configuration files and shows a difference."
     )
-    
+
     public class App implements Callable<Integer> {
+
+        @Option(
+                names = {"-f", "--format"},
+                description = "output format [default: stylish]",
+                paramLabel = "format",
+                defaultValue = "stylish")
+        String format;
+
+
+        @Parameters(
+                index = "0",
+                description = "path to first file",
+                paramLabel = "filepath1"
+        )
+        private String file1;
+
+        @Parameters(
+                index = "1",
+                description = "path to second file",
+                paramLabel = "filepath2"
+        )
+        private String file2;
 
         @Override
         public Integer call() { //логика приложения
