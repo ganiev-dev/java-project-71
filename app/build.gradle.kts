2334fa442e0e2c048981d9f277a13a4410558dca
rustam ganiev <psy12@yandex.ru>
2025-05-19 15:29:36 +0500

    sonar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("java")
     id("com.github.ben-manes.versions") version "0.52.0"
     id("org.sonarqube") version "6.2.0.5505"
+    id("jacoco")
     checkstyle
     application
 }
@@ -26,8 +27,8 @@ sonar {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    implementation ("org.junit.jupiter:junit-jupiter-api:5.5.1")
+    testImplementation ("org.junit.jupiter:junit-jupiter-api:5.9.3")  // API для тестов
+    testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.9.3") // Движок для запуска
     implementation ("info.picocli:picocli:4.7.7")
     annotationProcessor ("info.picocli:picocli-codegen:4.7.7")
     implementation ("com.fasterxml.jackson.core:jackson-databind:2.13.4.2")
@@ -35,4 +36,20 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+}
+jacoco {
+    toolVersion = "0.8.13"
+    reportsDirectory = layout.buildDirectory.dir("customJacocoReportDir")
+}
+tasks.jacocoTestReport {
+    reports {
+        xml.required = false
+        csv.required = false
+        html.outputLocation = layout.buildDirectory.dir("jacocoHtml")
+    }
 }
---
+++ app/src/test/java/SimpleTest.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+    @Test
+    void testSomething() {
+        System.out.println("Test is running!");
+    }
+}
